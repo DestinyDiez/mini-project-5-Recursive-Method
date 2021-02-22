6fc998ea055b0226ec326290d341b82155abd708
DestinyDiez <destiny.diezdeandino@patelhs.org>
2021-02-22 13:27:09 +0000

    i added a second scanner so that users can input their password for the Crest Bank account

--- Main.java
+++ Main.java
@@ -4,12 +4,22 @@ class Main {
    Scanner myBank = new Scanner(System.in);
    String userName;
 
-   System.out.println("Hello, welcome to Crest Bank! Please enter you username.");
+   System.out.println("Hello, welcome to Crest Bank! Please enter you username below.");
    userName = myBank.nextLine();
 
    System.out.println("Thank You, your username is: " + userName);
 
+   Scanner crestBank = new Scanner(System.in);
+   String passWord;
+
+   System.out.println("Now please enter your password below.");
+   passWord = crestBank.nextLine();
+
+   System.out.println("Thank you your password is: " + passWord);
+
    
+
+
 
   }
 }
