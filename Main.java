ded98dddba01a2e6677ac413bbba03318239f264
DestinyDiez <destiny.diezdeandino@patelhs.org>
2021-02-22 13:37:53 +0000

    i added 2 new print statements which states that they made it into their account

--- Main.java
+++ Main.java
@@ -1,23 +1,27 @@
 import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
+    //This will let users input a username
    Scanner myBank = new Scanner(System.in);
    String userName;
 
-   System.out.println("Hello, welcome to Crest Bank! Please enter you username below.");
+   System.out.println("-Hello, welcome to Crest Bank! Please enter you username below.");
    userName = myBank.nextLine();
 
    System.out.println("Thank You, your username is: " + userName);
 
+   //This will let users input a password
    Scanner crestBank = new Scanner(System.in);
    String passWord;
 
-   System.out.println("Now please enter your password below.");
+   System.out.println("-Now please enter your password below.");
    passWord = crestBank.nextLine();
 
-   System.out.println("Thank you your password is: " + passWord);
+   System.out.println("Thank you your password is: " + passWord );
+   System.out.println("-Welcome to your crest Bank account-");
+   System.out.println("Below is your remaining amount after last weekss transactions");
 
-   
+
 
 
 
